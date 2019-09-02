7a1fbbfb46053a2d5322b93d4f0bec56743c09d7
Krzysztof Wrobel <krzysztofw@cft-inc.net>
2019-09-01 19:22:44 -0500

    Added unittest functional

--- tests/func_test.py
+++ tests/func_test.py
@@ -1,12 +1,43 @@
+# First function al testing with selenium
+
 from selenium import webdriver
+import unittest
 
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        fb = "C:\\Users\\krzysztofw\\AppData\\Local\\Mozilla Firefox\\Firefox.exe"
+        self.browser = webdriver.Firefox(firefox_binary=fb)
 
-#cap = DesiredCapabilities().FIREFOX
-fb = "C:\\Users\\krzysztofw\\AppData\\Local\\Mozilla Firefox\\Firefox.exe"
-#ep = "C:\\dev\\geckodriver-v0.24.0-win64\\geckodriver.exe"
-#browser = webdriver.Firefox(capabilities=cap,firefox_binary=fb, executable_path=ep)
-browser = webdriver.Firefox(firefox_binary=fb)
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrive_it_latter(self):
+        # Guy did heard about new to do apps, she load it to check home page
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+        #He noticed the page tile and header 
+        self.assertIn( 'To-Do',self.browser.title)
+        self.fail('End of test')
+
+        #He is invited to enter a to-do item straight away
+
+        # He types "Buy pwacock feather" into a text box
+
+        # When hiting enter the page ypdtes and now the page looks like:
+        # 1: Buy peackock feathers as a an item in a to-do list
+
+        # Ther is still text box inviting her to add andother item
+        # He  adds 'Use peacock feathers to maje fly
+
+        # The page updates again and now shows both items on it
+
+        # Edith wonders whather the site will remember her list. she notice about
+        # generated a unique URL for her 
+
+        # He visits the URL - her to-do list is still there
+
+        # He is ok 
+
+if __name__ == '__main__':
+        unittest.main(warnings='ignore')
