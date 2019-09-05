6d117578eed84f5c5dcb77ec7569e74bd7d2a018
Krzysztof Wrobel <krzysztofw@cft-inc.net>
2019-09-05 14:23:40 -0500

    Func_test cheks now if we can input a to-do item

--- tests/func_test.py
+++ tests/func_test.py
@@ -18,18 +18,33 @@ class NewVisitorTest(unittest.TestCase):
 
         #He noticed the page tile and header 
         self.assertIn( 'To-Do',self.browser.title)
-        self.fail('End of test')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
         #He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # He types "Buy pwacock feather" into a text box
-
+        # He types "Buy peacock feather" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+        
         # When hiting enter the page ypdtes and now the page looks like:
         # 1: Buy peackock feathers as a an item in a to-do list
-
-        # Ther is still text box inviting her to add andother item
-        # He  adds 'Use peacock feathers to maje fly
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peackock feathers' for row in rows)
+            )
+        # There is still text box inviting her to add another item
+        # He adds 'Use peacock feathers to make fly'
+        self.fail('Finish the test')
+        
         # The page updates again and now shows both items on it
 
         # Edith wonders whather the site will remember her list. she notice about
