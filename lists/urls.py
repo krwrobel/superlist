50081e8a554df27018689b0ac7aa1debe0c6185b
Krzysztof Wrobel <krzysztofw@cft-inc.net>
2019-09-10 12:03:45 -0500

    refactor templates, fixed urls, css test

--- lists/urls.py
+++ lists/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
