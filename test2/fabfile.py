bf7f16405cc66761e6d15fe0f522c6dbb4ff2683
krwrobel <krzysztofw@cft-inc.net>
2019-09-24 10:40:37 -0500

    cleaning

--- test2/fabfile.py
+++
@@ -1,5 +0,0 @@
-from fabric import Connection
-result = Connection('superlist-staging.wblkrzysztof.site').run('uname -s', hide=True)
-msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
-
-print(msg)
