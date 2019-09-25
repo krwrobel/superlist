db6ec22db5413efba58bb639807356d067f8ab69
krwrobel <krzysztofw@cft-inc.net>
2019-09-25 10:19:48 -0500

    rename all item input ids names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,7 +13,7 @@ class ItemValidationTest(FunctionalTest):
         #Customer goes to home page and acciedentally tries to submit
         # a empty list item. Hits Enter on empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
         # Home page refreshes and ther ia an error message saying that list items cannot be blank
         self.wait_for(lambda: self.assertEqual(
@@ -22,12 +22,12 @@ class ItemValidationTest(FunctionalTest):
         ))
         
         # He tries again with item which works
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         
         #She decides to submit the empty item again
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
         #Gets similar warning
         self.wait_for(lambda: self.assertEqual(
@@ -35,7 +35,7 @@ class ItemValidationTest(FunctionalTest):
             "You can't have an empty list item"
            ))
         # He corrects it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
