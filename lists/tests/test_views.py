0caa14c3ec6d802dd50e77ad9e353d7acadd2797
krwrobel <krzysztofw@cft-inc.net>
2019-09-26 20:22:27 -0500

    add for for existing list, functional tests for hiding error display

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,10 @@ from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ( 
+    ItemForm, EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm,
+)
 from django.utils.html import escape
 from unittest import skip 
 
@@ -118,8 +121,8 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
     
     def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         
     def test_validation_errors_are_shown_on_home_page(self):
         response = self.client.post('/lists/new', data={'text': ''})
@@ -128,10 +131,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
     
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -140,7 +142,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
         
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
-        self.assertTemplateUseds(response,'list.html')
+        self.assertTemplateUsed(response,'list.html')
         self.assertEqual(Item.objects.all().count(),1)
