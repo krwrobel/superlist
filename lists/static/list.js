2653cb544937b3289e534eb6023206c1e31d917d
krwrobel <krzysztofw@cft-inc.net>
2019-10-02 11:18:47 -0500

    add on click error hiding

--- lists/static/list.js
+++ lists/static/list.js
@@ -3,4 +3,8 @@ window.Superlists.initialize = function () {
 	$('input[name="text"]').on('keypress', function () {    
 		$('.has-error').hide();
 	});
+	
+	$('input[name="text"]').on('click', function () {
+		$('.has-error').hide();
+	});
 };
