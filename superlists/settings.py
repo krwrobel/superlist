3a0a5227a4186efddb691a3308fba8283d6f56ec
krwrobel <krzysztofw@cft-inc.net>
2019-10-07 15:34:53 -0500

    working settting for login in

--- superlists/settings.py
+++ superlists/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kris00sparrow@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_POSRT = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 LOGGING = {
