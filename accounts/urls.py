712e31491b88a71a5357a6961ea1b2755aaa5bf2
krwrobel <krzysztofw@cft-inc.net>
2019-10-07 20:05:25 -0500

    add logout possibility

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from accounts import views
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send_login_email'),
     path('login', views.login, name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
