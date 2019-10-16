98414c5eace0191b96fbac46b073449925019478
krwrobel <krzysztofw@cft-inc.net>
2019-10-15 20:10:14 -0500

    login test for server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time 
 import os
 
@@ -14,9 +15,10 @@ class FunctionalTest(StaticLiveServerTestCase):
   
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary=self.fb)
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
     def tearDown(self):
         self.browser.quit()
         
