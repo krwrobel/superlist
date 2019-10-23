0bc1445ac56f3ba0d13543135882e8d4c407a5c4
krwrobel <krzysztofw@cft-inc.net>
2019-10-22 20:20:02 -0500

    lists can have owners

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
- 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+User = get_user_model()
                    
 class ItemModelTest(TestCase):
 
@@ -52,4 +52,12 @@ class ListModelTest(TestCase):
             
     def test_get_absolute_url(self):
         list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')        
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+        
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email = 'a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+        
+    def test_list_owner_is_optional(self):
+        List.objects.create() # should not rise
